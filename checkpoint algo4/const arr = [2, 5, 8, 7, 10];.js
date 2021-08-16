fc37ff8e02e1cd114634e3c5d0edf55e90aa5c53
HASSEN1990 <88149193+HASSEN1990@users.noreply.github.com>
2021-08-16 15:54:21 +0100

    Add files via upload

---
+++ checkpoint algo4/const arr = [2, 5, 8, 7, 10];.js
@@ -0,0 +1,16 @@
+const arr = [2, 5, 8, 7, 10];
+
+const insertionSort = arr => {
+    for(let i=1;i<arr.length; i++){
+        let curr = arr[i];
+        let j = i - 1;
+        while (j>=0 && arr[j]>curr){
+            arr[j+1]=arr[j];
+            j--
+        }
+        arr[j+1] = curr;
+    }
+    return arr;
+}
+insertionSort(arr);
+console.log(arr);
